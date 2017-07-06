a449b95d862143ba34062122b2d796325b51e960
Ingo Albers <ingo.albers@live.de>
2017-07-06 16:14:14 +0200

    Render all daily menus in show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,14 +1,6 @@
 class MenusController < ActionController::Base
   def show
-    date = if params[:date]
-            Date.parse(params[:date])
-          else
-            Date.today
-          end
-
-    daily_menu = DailyMenu.find_by(date: date.beginning_of_day)
-
-    render json: { l(date) => daily_menu.content }
+    render json: DailyMenu.where(date: Date.today.beginning_of_day)
   rescue StandardError
     render json: { l(date) => 'error' }
   end
