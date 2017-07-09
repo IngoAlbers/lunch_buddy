fd1f4be16d5ffb1a0198076d5e60934e4dcae5f8
Ingo Albers <ingo.albers@live.de>
2017-07-09 21:20:51 +0200

    Add cascade to foreign key

--- db/migrate/20170706125048_add_daily_menus.rb
+++ db/migrate/20170706125048_add_daily_menus.rb
@@ -3,7 +3,7 @@ class AddDailyMenus < ActiveRecord::Migration[5.1]
     create_table :daily_menus do |t|
       t.datetime :date, null: false
       t.string :content, null: false
-      t.references :restaurant, index: true, foreign_key: true, null: false
+      t.references :restaurant, index: true, null: false, foreign_key: { on_delete: :cascade }
     end
 
     add_index :daily_menus, %i[date restaurant_id], unique: true
