45b5d8c7027038611c8acc7d0f5b1b6963a26b07
Ingo Albers <ingo.albers@live.de>
2017-07-15 23:58:12 +0200

    Don't modify frozen strings

--- app/models/restaurant/lilly_jo.rb
+++ app/models/restaurant/lilly_jo.rb
@@ -43,12 +43,12 @@ module Restaurant
     end
 
     def sanitize(str)
-      str = str.force_encoding('ASCII-8BIT')
-               .gsub(/#{"\x00|\xFE|\xFF".force_encoding("ASCII-8BIT")}/, '')
-               .gsub(/#{"\xE4".force_encoding("ASCII-8BIT")}/, 'aaee')
-               .gsub(/#{"\xF6".force_encoding("ASCII-8BIT")}/, 'ooee')
-               .gsub(/#{"\xFC".force_encoding("ASCII-8BIT")}/, 'uuee')
-               .gsub(/#{"\xE9".force_encoding("ASCII-8BIT")}/, 'e')
+      str = str.dup.force_encoding('ASCII-8BIT')
+               .gsub(/#{"\x00|\xFE|\xFF".dup.force_encoding("ASCII-8BIT")}/, '')
+               .gsub(/#{"\xE4".dup.force_encoding("ASCII-8BIT")}/, 'aaee')
+               .gsub(/#{"\xF6".dup.force_encoding("ASCII-8BIT")}/, 'ooee')
+               .gsub(/#{"\xFC".dup.force_encoding("ASCII-8BIT")}/, 'uuee')
+               .gsub(/#{"\xE9".dup.force_encoding("ASCII-8BIT")}/, 'e')
 
       # Workaround to make sure that 'ue' in 'sauer' doesn't turn to the umlaut
       str.gsub('aaee', 'ä')
