dc457bf50bbcc83341cc55f3afd63351194b006b
Ingo Albers <ingo.albers@live.de>
2017-08-14 21:20:50 +0200

    Require subclass restaurants when gathering daily menus

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,12 @@
 
 module Restaurant
   def self.gather_daily_menus(date = Date.today)
+    Restaurant.require_subclasses
     Restaurant::BaseRestaurant.subclasses.each { |restaurant| restaurant.gather_daily_menu(date) }
+  end
+
+  # Needed because rake tasks don't eager load the models even in production
+  def self.require_subclasses
+    Dir[File.expand_path('restaurant/*.rb', __dir__)].each { |src| require src }
   end
 end
